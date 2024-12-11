16404e2ae98fc2d52d8579970a95a83c0d10b885
Souza284 <kanaan.pedro2@gmail.com>
2024-12-11 12:04:55 -0300

    Commit my first project

---
+++ First_Project/Program.cs
@@ -0,0 +1,47 @@
+﻿// Screen Sound!
+
+var Welcome_msg = "Welcome to Screen Sound!";
+
+void Show_msg()
+{
+        Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+}
+
+Show_msg();
+Console.WriteLine("");
+
+void Show_menu()
+{
+    Console.WriteLine(Welcome_msg);
+    Console.WriteLine("\nType 1 to register a band: ");
+    Console.WriteLine("Type 2 to show all the bands: ");
+    Console.WriteLine("Type 3 to evaluate a band: ");
+    Console.WriteLine("Type 4 to show a note from a band: ");
+    Console.WriteLine("Type 0 to exit: \n");
+
+    Console.Write("Option: ");
+    int Option = Convert.ToInt32(Console.ReadLine());
+
+    switch (Option)
+    {
+        case 1: 
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 0:
+            break;
+        default: Console.WriteLine("Not valid option...");
+            break;
+    }
+}
+Show_menu();

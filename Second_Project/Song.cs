cc8ed46b2eacaf02276c6694f6abfc85f56befcf
Souza284 <kanaan.pedro2@gmail.com>
2024-12-20 17:40:07 -0300

    Second Project

---
+++ Second_Project/Song.cs
@@ -0,0 +1,10 @@
+namespace Second_Project
+{
+    public class Song
+    {
+        public string? name;
+        public string? artist;
+        public double time;
+        public bool avaiable;
+    }
+}

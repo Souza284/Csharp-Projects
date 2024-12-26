1df7d009ebd6eb0d928981782904b451a24b66ab
Souza284 <kanaan.pedro2@gmail.com>
2024-12-26 14:31:33 -0300

    .

--- First_Project/Models/Review.cs
+++ First_Project/Models/Review.cs
@@ -2,12 +2,28 @@ namespace First_Project.Models
 {
     internal class Review
     {
-        public int Rate { get;}
-        public Review(int rate)
+        public double Rate { get; }
+        public Review(double rate)
         {
+            if (rate > 10)
+            {
+                rate = 10;
+            }
+            else
+            {
+                if (rate < 0)
+                {
+                    rate = 0;
+                }
+            }
             Rate = rate;
-
         }
-        
+
+        public static Review Convert(string text)
+        {
+            var review = Double.Parse(text);
+            return new Review(review);
+        }
+
     }
 }

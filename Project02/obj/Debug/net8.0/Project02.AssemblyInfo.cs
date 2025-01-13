efcb63b8bdf8c4ea2ecba592313a8305e28abaa3
Souza284 <kanaan.pedro2@gmail.com>
2025-01-13 20:59:56 -0300

    update

--- Project02/obj/Debug/net8.0/Project02.AssemblyInfo.cs
+++ Project02/obj/Debug/net8.0/Project02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3988c89f34464dbf1ccc7f3bb5fc1d0a61a140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529ab80b840ed215838624d29708a42c54d7de60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

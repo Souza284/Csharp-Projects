e7fc427e64397c62f201ec7ebfe0ac5dd4e6f338
Souza284 <kanaan.pedro2@gmail.com>
2025-01-15 14:04:47 -0300

    .

--- Project03/Models/CurrentAccount.cs
+++ Project03/Models/CurrentAccount.cs
@@ -4,13 +4,36 @@ namespace Project03.Models
     {
         public int Number { get; set; }
         public int Agency { get; set; }
-        public double Balance { get; set; }
+        public double Balance { get; private set; }
         public Client? Holder { get; set; }
 
         public CurrentAccount(int agency, int number)
         {
             Agency = agency;
             Number = number;
+        }
+
+        public void Deposit(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Deposit value must to be greater than 0.", nameof(value));
+            }
+
+            Balance += value;
+        }
+
+        public void WithDraw(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Deposit value must to be greater than 0.", nameof(value));
+            }
+            if (value > Balance)
+            {
+                throw new ArgumentException("Insufficient balance.");
+            }
+            Balance += value;
         }
     }
 }

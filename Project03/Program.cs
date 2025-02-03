8885849368cb4020976916cba0424f38ef2983ba
Souza284 <kanaan.pedro2@gmail.com>
2025-02-03 17:42:32 -0300

    .

--- Project03/Program.cs
+++ Project03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Text.Unicode;
+using Project03.Models;
 
 
 partial class Program
@@ -25,8 +26,40 @@ partial class Program
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
-                Console.WriteLine(line);
+                var currentAccount = ConvertStringToCurrentAccount(line!);
+                var message = $"Number: {currentAccount.Number} Agency: {currentAccount.Agency} Balance: {currentAccount.Balance} Client: {currentAccount.Holder.Name}"; 
+
+                Console.WriteLine(message);
+                //Console.WriteLine(line);
             }
         }
+        Console.ReadLine();
+    }
+
+    static CurrentAccount ConvertStringToCurrentAccount(string line)
+    {
+
+        //375 4644 2483.13 Jonatan
+
+        var fields = line.Split(',');
+
+        var agency = fields[0];
+        var number = fields[1];
+        var balance = fields[2];
+        var holder = fields[3];
+
+        //the fields above are strings
+
+        var agencyInt = int.Parse(agency);
+        var numberInt = int.Parse(number);
+        var balanceDouble = double.Parse(balance);
+        var client = new Client();
+        client.Name = holder;
+
+        var result = new CurrentAccount(agencyInt, numberInt);
+        result.Deposit(balanceDouble);
+        result.Holder = client;
+
+        return result;
     }
 }
